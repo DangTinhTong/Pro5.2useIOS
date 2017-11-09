394d640ef2785259dd53a52b99f9d5d38cd54c1d
admin <dangtinhtong@gmail.com>
2017-11-09 13:05:42 +0700

    Initial Commit

---
+++ Prog5.2ForLoop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prog5.2ForLoop
+//
+//  Created by admin on 11/9/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Prog5.2ForLoop/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Prog5.2ForLoop
+//
+//  Created by admin on 11/9/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
