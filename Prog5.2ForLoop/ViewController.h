d43d1d4b4e34cd4b58328eebcf8334876a8a713a
admin <dangtinhtong@gmail.com>
2017-11-09 14:17:29 +0700

    first commit

--- Prog5.2ForLoop/ViewController.h
+++ Prog5.2ForLoop/ViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *resultTextField;
 
+- (IBAction)resultButton:(id)sender;
 
 @end
 
--- Prog5.2ForLoop/ViewController.m
+++ Prog5.2ForLoop/ViewController.m
@@ -9,7 +9,11 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+/*@property (weak, nonatomic) IBOutlet UITextField *numberTextField;
+ @property (weak, nonatomic) IBOutlet UITextField *resultTextField;
+ 
+ - (IBAction)resultButton:(id)sender;
+ */
 @end
 
 @implementation ViewController
@@ -17,13 +21,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+     _numberTextField.placeholder = @"200";
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    
 }
 
 
+- (IBAction)resultButton:(id)sender {
+   // int number=[[self.numberTextField text] intValue];
+   // int result=0;
+  //  for(number=1; number<=200;number+1)
+        
+     //   result+=number;
+    //[self Resultofexpression];
+   // _numberTextField.placeholder = @"200";
+    NSString *myResult =[NSString stringWithFormat:@"%i",[self Resultofexpression]];
+    self.resultTextField.text=myResult;
+}
+-(int) Resultofexpression
+{
+    int number=[[self.numberTextField text] intValue];
+
+    int result=0;
+    
+    for(number=1; number<=200;number++)
+        
+        result+=number;
+    return result;
+}
 @end
+
+
